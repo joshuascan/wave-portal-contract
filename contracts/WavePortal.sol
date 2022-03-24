8b378de6934423e949f55e0b10c1945ecc9e0cae
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-03-23 17:50:29 -0700

    adds randomness and spam prevention

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,9 @@ import "hardhat/console.sol";
 
 contract WavePortal {
   uint256 totalWaves;
-  mapping(address => uint) userWaveCount;
+  uint256 private seed;
+  mapping(address => uint256) userWaveCount;
+  mapping(address => uint256) public lastWavedAt;
 
   event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -20,24 +22,39 @@ contract WavePortal {
 
   constructor() payable {
     console.log("We have been constructed!");
+    seed = (block.timestamp + block.difficulty) % 100;
   }
 
   function wave(string memory _message) public {
+    require(
+      lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+      "Wait 15 minutes"
+    );
+
+    lastWavedAt[msg.sender] = block.timestamp;
+
     totalWaves += 1;
     userWaveCount[msg.sender] += 1;
     console.log("%s waved w/ message %s:", msg.sender, _message);
 
     waves.push(Wave(msg.sender, _message, block.timestamp));
 
+    seed = (block.timestamp + block.difficulty + seed) % 100;
+
+    if (seed < 50) {
+      console.log("%s won!", msg.sender);
+
+      uint256 prizeAmount = 0.0001 ether;
+      require(
+        prizeAmount <= address(this).balance,
+        "Trying to withdraw more money than the sontract has."
+      );
+      (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+      require(success, "Failed to withdraw money from contract.");
+    }
+
     emit NewWave(msg.sender, block.timestamp, _message);
 
-    uint256 prizeAmount = 0.0001 ether;
-    require(
-      prizeAmount <= address(this).balance,
-      "Trying to withdraw more money than the sontract has."
-    );
-    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-    require(success, "Failed to withdraw money from contract.");
   }
 
   function getAllWaves() public view returns (Wave[] memory) {
@@ -45,12 +62,10 @@ contract WavePortal {
   }
 
   function getTotalWaves() public view returns (uint256) {
-    console.log("We have %d total waves!", totalWaves);
     return totalWaves;
   }
 
   function getUserWaveCount(address _userAddress) public view returns (uint) {
-    console.log("%s has waved %d times!", _userAddress, userWaveCount[_userAddress]);
     return userWaveCount[_userAddress];
   }
 }
